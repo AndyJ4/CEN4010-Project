70c3c3686d180081f3e1a8c7ce463a1364040e48
Jay25102 <123430083+Jay25102@users.noreply.github.com>
2023-03-28 12:08:15 -0400

    Add files via upload

--- bookstoreapi/src/main/java/com/bookstore/bookstoreapi/Repositories/BookBrowsingRepository.java
+++ bookstoreapi/src/main/java/com/bookstore/bookstoreapi/Repositories/BookBrowsingRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface BookBrowsingRepository extends JpaRepository<BookBrowsingEntity, Integer> {
+
+    BookBrowsingEntity getReferenceById(Integer id);
+
     List<BookBrowsingEntity> findByGenre(String genre);
     List<BookBrowsingEntity> findTop10ByOrderByCopiesDesc();
     List<BookBrowsingEntity> findByRatingGreaterThan(Double rating);
